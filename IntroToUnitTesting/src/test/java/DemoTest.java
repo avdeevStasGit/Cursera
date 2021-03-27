fb962da459f72e680defdea19aadfe85fba0d2d4
Stas Avdeev <d1g1texavd@gmail.com>
2021-03-27 22:50:57 +0300

    Update tests

--- IntroToUnitTesting/src/test/java/DemoTest.java
+++ IntroToUnitTesting/src/test/java/DemoTest.java
@@ -1,3 +1,13 @@
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.*;
+
 /**
  * The class containing your tests for the {@link Demo} class.  Make sure you
  * test all methods in this class (including both 
@@ -5,5 +15,48 @@
  * {@link Demo#isTriangle(double, double, double)}).
  */
 public class DemoTest {
+
+
+    @Test
+    public void test_is_triangle_1() {
+        assertTrue(Demo.isTriangle(3,4,5));
+    }
+
+    @Test
+    public void test_is_triangle_2() {
+        assertTrue(Demo.isTriangle(6,12,15));
+    }
+
+    @Test
+    public void test_is_triangle_3() {
+        assertTrue(Demo.isTriangle(8,6,12));;
+    }
+
+    @Test
+    public void test_is_NOT_triangle_1() {
+        assertFalse(Demo.isTriangle(-3,45,34));
+    }
+
+    @Test
+    public void test_is_NOT_triangle_2() {
+        assertFalse(Demo.isTriangle(0,7,12));
+    }
+
+    @Test
+    public void test_MAIN_program() {
+        ByteArrayInputStream in = new ByteArrayInputStream("5\n12\n13\n".getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String consoleOutput = "Enter side 1:\n";
+        consoleOutput += "Enter side 2:\n";
+        consoleOutput += "Enter side 3:\n";
+        consoleOutput += "This is a triangle.\n";
+
+        assertEquals(consoleOutput,out.toString());
+    }
+
 
 }
