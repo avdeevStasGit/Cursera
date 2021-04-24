26eccaa246c5035f06a4a5de352ddb9aa356e306
Stas Avdeev <d1g1texavd@gmail.com>
2021-04-24 18:00:53 +0300

    Update

---
+++ CoffeeMaker_JUnit_Golden/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -0,0 +1,41 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CoffeeMakerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void addRecipe() {
+    }
+
+    @Test
+    public void deleteRecipe() {
+    }
+
+    @Test
+    public void editRecipe() {
+    }
+
+    @Test
+    public void addInventory() {
+    }
+
+    @Test
+    public void checkInventory() {
+    }
+
+    @Test
+    public void makeCoffee() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
