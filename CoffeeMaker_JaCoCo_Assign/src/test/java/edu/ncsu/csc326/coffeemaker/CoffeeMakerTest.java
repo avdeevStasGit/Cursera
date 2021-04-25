18b4b6133b6f4e9c4d38d2544b29e12156ce4992
Stas Avdeev <d1g1texavd@gmail.com>
2021-04-25 13:25:12 +0300

    Update task 1.0

--- CoffeeMaker_JaCoCo_Assign/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ CoffeeMaker_JaCoCo_Assign/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -1,7 +1,10 @@
 package edu.ncsu.csc326.coffeemaker;
 
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 import junit.framework.TestCase;
-
+import org.junit.Assert;
+import org.junit.Test;
 
 
 /**
@@ -15,74 +18,206 @@ import junit.framework.TestCase;
 
 public class CoffeeMakerTest extends TestCase {
 	
-	private Recipe r1;
-	private Recipe r2;
-	private Recipe r3;
-	private Recipe r4;
-	private Recipe r5;
-	private CoffeeMaker cm;
+	private Recipe recipe0;
+	private Recipe recipe1;
+	private Recipe recipe2;
+	private Recipe recipe3;
+	private CoffeeMaker coffeeMaker;
+	private Inventory inventory;
 	private RecipeBook recipeBookStub;
 	private Recipe [] stubRecipies; 
 	
 	protected void setUp() throws Exception {
 		
-		cm = new CoffeeMaker();
+		coffeeMaker = new CoffeeMaker();
 		
 		//Set up for r1
-		r1 = new Recipe();
-		r1.setName("Coffee");
-		r1.setAmtChocolate("0");
-		r1.setAmtCoffee("3");
-		r1.setAmtMilk("1");
-		r1.setAmtSugar("1");
-		r1.setPrice("50");
+		recipe0 = new Recipe();
+		recipe0.setName("Coffee");
+		recipe0.setAmtChocolate("0");
+		recipe0.setAmtCoffee("3");
+		recipe0.setAmtMilk("1");
+		recipe0.setAmtSugar("1");
+		recipe0.setPrice("50");
 		
 		//Set up for r2
-		r2 = new Recipe();
-		r2.setName("Mocha");
-		r2.setAmtChocolate("20");
-		r2.setAmtCoffee("3");
-		r2.setAmtMilk("1");
-		r2.setAmtSugar("1");
-		r2.setPrice("75");
+		recipe1 = new Recipe();
+		recipe1.setName("Mocha");
+		recipe1.setAmtChocolate("20");
+		recipe1.setAmtCoffee("3");
+		recipe1.setAmtMilk("1");
+		recipe1.setAmtSugar("1");
+		recipe1.setPrice("75");
 		
 		//Set up for r3
-		r3 = new Recipe();
-		r3.setName("Latte");
-		r3.setAmtChocolate("0");
-		r3.setAmtCoffee("3");
-		r3.setAmtMilk("3");
-		r3.setAmtSugar("1");
-		r3.setPrice("100");
+		recipe2 = new Recipe();
+		recipe2.setName("Latte");
+		recipe2.setAmtChocolate("0");
+		recipe2.setAmtCoffee("3");
+		recipe2.setAmtMilk("3");
+		recipe2.setAmtSugar("1");
+		recipe2.setPrice("100");
 		
 		//Set up for r4
-		r4 = new Recipe();
-		r4.setName("Hot Chocolate");
-		r4.setAmtChocolate("4");
-		r4.setAmtCoffee("0");
-		r4.setAmtMilk("1");
-		r4.setAmtSugar("1");
-		r4.setPrice("65");
-		
-		//Set up for r5 (added by MWW)
-		r5 = new Recipe();
-		r5.setName("Super Hot Chocolate");
-		r5.setAmtChocolate("6");
-		r5.setAmtCoffee("0");
-		r5.setAmtMilk("1");
-		r5.setAmtSugar("1");
-		r5.setPrice("100");
+		recipe3 = new Recipe();
+		recipe3.setName("Hot Chocolate");
+		recipe3.setAmtChocolate("4");
+		recipe3.setAmtCoffee("0");
+		recipe3.setAmtMilk("1");
+		recipe3.setAmtSugar("1");
+		recipe3.setPrice("65");
 
-		stubRecipies = new Recipe [] {r1, r2, r3};
+		stubRecipies = new Recipe [] {recipe0, recipe1, recipe2};
 		
 		super.setUp();
 	}
-	
-	
-	// put your tests here!
-	
+
+
+	@Test
+	public void testAddRecipe() throws RecipeException {
+		recipe3 = new Recipe();
+		recipe3.setName("Hot Chocolate");
+		recipe3.setAmtChocolate("4");
+		recipe3.setAmtCoffee("0");
+		recipe3.setAmtMilk("1");
+		recipe3.setAmtSugar("1");
+		recipe3.setPrice("65");
+
+		coffeeMaker.addRecipe(recipe3);
+	}
+
+	@Test
+	public void testDeleteRecipe1() {
+	}
+
+	@Test
+	public void testDeleteRecipe2(){
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.deleteRecipe(0);
+		Assert.assertNotSame(recipe1, coffeeMaker.getRecipes()[0]);
+		Assert.assertNull(coffeeMaker.getRecipes()[0]);
+	}
+
+	@Test
+	public void testEditRecipe() {
+	}
+
+	//--------------------------------------------------
+	//testInventoryStart
+	//--------------------------------------------------
+
+	@Test
+	public void testAddInventory() throws InventoryException {
+		coffeeMaker.addInventory("5", "8", "0", "8");
+	}
+
+	//Если пользователь вводит буквенный символ, пользователю будет предложено ввести сумму повторно.
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException11() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "asdf", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException12() throws InventoryException {
+		coffeeMaker.addInventory("asdf", "5", "4", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException13() throws InventoryException {
+		coffeeMaker.addInventory("4", "asdf", "4", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException14() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "4", "asdf");
+	}
+
+	//Если пользователь выбирает отрицательное или не целое число, пользователю будет предложено ввести сумму повторно.
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException21() throws InventoryException {
+		coffeeMaker.addInventory("4", "-5", "3", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException22() throws InventoryException {
+		coffeeMaker.addInventory("-4", "5", "3", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException23() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "-3", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException24() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "3", "-3");
+	}
+
+	//Если пользователь выбирает отрицательное или не целое число, пользователю будет предложено ввести сумму повторно.
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException31() throws InventoryException {
+		coffeeMaker.addInventory("4", "5.4", "3", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException32() throws InventoryException {
+		coffeeMaker.addInventory("4.0", "5.4", "3", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException33() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "3.6", "3");
+	}
+
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException34() throws InventoryException {
+		coffeeMaker.addInventory("4", "5", "3", "3.7");
+	}
+	//--------------------------------------------------
+	//testInventoryEnd
+	//--------------------------------------------------
+
+	// пользователю будет показан список ингредиентов в кофеварке.
+	@Test
+	public void testCheckInventory() {
+		inventory.setMilk(10);
+		inventory.setChocolate(10);
+		inventory.setCoffee(10);
+		inventory.setSugar(10);
+		Assert.assertEquals("Coffee: 10\n" +
+				"Milk: 10\n" +
+				"Sugar: 10\n" +
+				"Chocolate: 10\n" , coffeeMaker.checkInventory());
+	}
+
+	//Когда мы делаем кофе, выбираем действующий рецепт и платим больше, чем стоимость кофе. Сдача 25.
+	@Test
 	public void testMakeCoffee() {
-		assertTrue(true);
+		coffeeMaker.addRecipe(recipe0);
+		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
+
+	}
+
+	//Кофеварка проверит, достаточно ли ингредиентов в инвентаре для приготовления выбранного напитка.
+	//Если запасов недостаточно для приготовления напитка, отобразится сообщение, деньги пользователя будут возвращены
+	@Test
+	public void testMakeCoffee1() {
+		inventory.setMilk(1);
+		inventory.setChocolate(1);
+		inventory.setCoffee(1);
+		inventory.setSugar(1);
+
+		coffeeMaker.addRecipe(recipe0);
+		assertEquals(50, coffeeMaker.makeCoffee(0, 50));
+
+	}
+
+	@Test
+	public void testGetRecipes() {
+		coffeeMaker.addRecipe(recipe1);
+		Recipe[] recipes = new Recipe[3];
+		recipes[0] = recipe1;
+		Assert.assertArrayEquals(recipes ,coffeeMaker.getRecipes());
 	}
 
 	
